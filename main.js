f55ac440f28a7e2f76fbd8793b92b860e364edd5
Veerockss <90184041+Veerockss@users.noreply.github.com>
2022-01-12 16:08:29 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var names = [
+    "Veer Sanghavi (Me)", "Milind Sanghavi (Dad)", "Jyoti Taparia (Mom)"
+];
+var images =[
+    "veer.png", "dad.png", "mom.png"
+];
+var i=0;
+function nextslide(){
+  document.getElementById("p1").innerHTML=names[i];
+  document.getElementById("members").src=images[i];
+  i++;
+}
